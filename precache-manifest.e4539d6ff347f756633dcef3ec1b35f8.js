72042ec678cd61ce2630e0c518fe44a69186df2d
Shreyans13 <shreyans1313@gmail.com>
2021-01-14 16:23:40 +0530

    🚀 deploy

---
+++ precache-manifest.e4539d6ff347f756633dcef3ec1b35f8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00e1f2e2f840cdfae2bc",
+    "url": "/vue-firebase/css/chunk-vendors.39b842e3.css"
+  },
+  {
+    "revision": "54e165217ebcd5f627d05ad73a781ea1",
+    "url": "/vue-firebase/img/Simple.jpeg"
+  },
+  {
+    "revision": "44b31b544b9ca3c736788200b4fd120d",
+    "url": "/vue-firebase/img/firebase.44b31b54.png"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/vue-firebase/img/vue.82b9c7a5.png"
+  },
+  {
+    "revision": "2473d454268365abaeebbdae326578cd",
+    "url": "/vue-firebase/index.html"
+  },
+  {
+    "revision": "0d9d6f6432fb70de6a5f",
+    "url": "/vue-firebase/js/app.0b4c1215.js"
+  },
+  {
+    "revision": "00e1f2e2f840cdfae2bc",
+    "url": "/vue-firebase/js/chunk-vendors.3a22456b.js"
+  },
+  {
+    "revision": "a7b985238384dc336f0a16d577f68212",
+    "url": "/vue-firebase/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-firebase/robots.txt"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,66 @@
+importScripts("/vue-firebase/precache-manifest.e4539d6ff347f756633dcef3ec1b35f8.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+
+workbox.setConfig({
+  debug: true
+});
+
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+// Caching Fonts
+
+workbox.routing.registerRoute(
+  new RegExp("https://fonts.(?:googleapis|gstatic).com/(.*)"),
+  workbox.strategies.cacheFirst({
+    cacheName: "googleapis",
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxEntries: 30
+      })
+    ],
+    method: "GET",
+    cacheableResponse: { statuses: [0, 200] }
+  })
+);
+
+// workbox.routing.registerRoute(
+//     new RegExp("https://fonts.(?:googleapis|gstatic).com/(.*)"),
+//     new CacheFirst({
+//         cacheName: "googleapis",
+//         plugins: [
+//             new ExpirationPlugin({
+//                 maxEntries: 30
+//             })
+//         ],
+//         method: 'GET',
+//         cacheableResponse: {statuses: [0, 200]}
+//     })
+// )
+
+// Push Notification
+let click_open_url;
+self.addEventListener("push", event => {
+  let push_message = event.data.text();
+  click_open_url = "https://www.google.com";
+  const options = {
+    body: push_message,
+    icon: "./img/logo.82b9c7a5.png",
+    image: "./img/Simple.jpeg",
+    vibrate: [200, 100, 200, 100, 200, 100, 200, 100],
+    tag: "vibration-sample"
+  };
+  event.waitUntil(
+    self.registration.showNotification("my-notification", options)
+  );
+});
+
+self.addEventListener("notificationclick", event => {
+  const clickedNotification = event.notification;
+  clickedNotification.close();
+  if (click_open_url) {
+    const promiseChain = clients.openWindow(click_open_url);
+    event.waitUntil(promiseChain);
+  }
+});
+
